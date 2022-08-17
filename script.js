5e29836b425b75c68f75989983a5de82bc110c57
Vishnupriya S <108866164+SmileVP@users.noreply.github.com>
2022-08-17 22:06:52 +0530

    Update script.js

--- script.js
+++ script.js
@@ -1,5 +1,5 @@
 //Question 1:
-
+a)For in
 var arr={
     "name":"Vishnupriya",
     "age":"24"
@@ -7,7 +7,7 @@ var arr={
 for(var key in arr){
     console.log(key,arr[key]);
 }
-
+b)For loop
 
 var res=[{"name":"Balaji","age":"21"},{"name":"LathaSampath","age":"22"}];
 for(let i=0;i<res.length;i++){
